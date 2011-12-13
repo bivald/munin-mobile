ee4367dad2538fa43bbe95b77f971538fda3b031
root <root@stats.chattarna.se>
2011-12-13 11:37:44 +0100

    Updated to jQuery Mobile 1.0, css fix and iPhone landscape bug fix

--- mobile-www/js/munin-mobile.js
+++ mobile-www/js/munin-mobile.js
@@ -14,3 +14,13 @@ function doJump(whereTo) {
                setTimeout("toppx = $('#" + whereTo  + "').position()['top']; $('body').scrollTop(toppx)",100) 
            }
 }
+
+if (navigator.userAgent.match(/iPhone/i)) {
+    $(window).bind('orientationchange', function(event) {
+        if (window.orientation == 90 || window.orientation == -90 || window.orientation == 270) {
+            $('meta[name="viewport"]').attr('content', 'height=device-width,width=device-height,initial-scale=1.0,maximum-scale=10.0');
+        } else {
+            $('meta[name="viewport"]').attr('content', 'height=device-height,width=device-width,initial-scale=1.0,maximum-scale=1.0');
+        }
+    }).trigger('orientationchange');
+}
