c4517f32ff91c1e628a74f4c18e9d8613448dbff
root <root@stats.chattarna.se>
2011-12-13 12:20:04 +0100

    Munin Mobile no longer resizes the images, you might need to clear your /var/www/munin/mobile/domain/*.png to regenerat

--- update-mobile.sh
+++ update-mobile.sh
@@ -8,19 +8,6 @@
 /usr/share/munin/munin-html --config=/etc/munin/munin-mobile.conf
 
 # Find images in mobile dir, files, *.png, modified between 0 and 3 minutes, not including images, resize with mogrify
-find /var/www/munin/mobile/ -type f -name "*.png"  -mmin +0 -mmin -3  -not -wholename "*images/*" -exec mogrify -resize 300 "{}" \;
 
-# Old, obsolete labour intensive resize
-
-exit
-cd /var/www/munin/mobile
-
-for file in `dir -d *` ; do
-    if [ -d "/var/www/munin/mobile/$file" ]
-    then
-        if [ $file != "images" ] # Dont resize our template images, we probably should check css/js here as well...
-            then
-                /usr/bin/mogrify -resize 300 /var/www/munin/mobile/$file/*.png
-            fi
-    fi    
-done
+# We no longer resize our images
+# find /var/www/munin/mobile/ -type f -name "*.png"  -mmin +0 -mmin -3  -not -wholename "*images/*" -exec mogrify -resize 300 "{}" \;
